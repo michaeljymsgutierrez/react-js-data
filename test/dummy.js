b393ac3f1317cb6d7cb7ad6e1002311fb748b29e
michaeljymsgutierrez <leeez711@gmail.com>
2020-03-31 22:34:10 +0800

    Added initial dummy for data modeling

---
+++ test/dummy.js
@@ -0,0 +1,11 @@
+const _this = window;
+
+this.reactData.model({
+  type: "user",
+
+  firstName: "string",
+  lastName: "string",
+  birthDate: "date",
+  mobileNumber: "number",
+  employmentStatus: "boolean"
+});
