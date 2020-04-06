51ee17ceea26b3090fe33f554edd429e3cedbbfc
michaeljymsgutierrez <leeez711@gmail.com>
2020-04-06 22:28:50 +0800

    Moved host to private function and exposed to global

--- test/index.js
+++ test/index.js
@@ -18,14 +18,27 @@
     }
   };
 
+  /* Host Privacy
+   *
+   */
+  const _host = {
+    host: window.location.origin,
+
+    setHost: function(host) {
+      this.host = host;
+    },
+
+    getHost: function() {
+      return this.host;
+    }
+  };
+
   const _reactData = {
     setNamespace: _namespace.setNamespace,
     getNamespace: _namespace.getNamespace,
 
-    host: window.location.origin,
-    setHost: function(host) {
-      this.host = host;
-    },
+    setHost: _host.setHost,
+    getHost: _host.getHost,
 
     store: {
       collections: {},
