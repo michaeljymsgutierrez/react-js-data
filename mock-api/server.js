569e9fc65bf38481ffa62630a66661fadbc103d5
michaeljymsgutierrez <leeez711@gmail.com>
2020-04-11 20:55:24 +0800

    Updated express server script

--- mock-api/server.js
+++ mock-api/server.js
@@ -3,8 +3,10 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import fs from "fs";
 
 const app = express();
+const currentPath = __dirname;
 
 app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,6 +16,22 @@ app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.json({ success: 200 });
+});
+
+const users = JSON.parse(
+  fs.readFileSync(`${currentPath}/json/users.json`).toString()
+);
+
+const customerProfiles = JSON.parse(
+  fs.readFileSync(`${currentPath}/json/customer-profiles.json`).toString()
+);
+
+app.get("/api/v1/users", (req, res) => {
+  res.json(users);
+});
+
+app.get("/api/v1/customer-profiles", (req, res) => {
+  res.json(customerProfiles);
 });
 
 app.listen("8081", () => {
